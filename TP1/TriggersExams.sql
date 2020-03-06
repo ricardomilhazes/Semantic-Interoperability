52dd13bc89d4ce18476b478e83081b1bbcd0f922
ricardomilhazes <rmilhazesveloso@gmail.com>
2020-03-06 01:56:45 +0000

    Trigger

--- TP1/TriggersExams.sql
+++ TP1/TriggersExams.sql
@@ -6,6 +6,6 @@ FOR EACH ROW
 INSERT INTO worklist
 SELECT NEW.idRequest,NEW.State,NEW.Date,
 		NEW.Medical_Act,NEW.User_idUser,NEW.Report,NEW.Notes
-        name,idProcess,address,mobile,notes
+        Name,idProcess,Address,Mobile
 FROM user
 WHERE NEW.User_idUser = user.idUser;
