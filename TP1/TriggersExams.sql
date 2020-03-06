b8d54aa67619b4fd109d659a92965a00c3b20a55
ricardomilhazes <rmilhazesveloso@gmail.com>
2020-03-06 02:15:34 +0000

    Trigger

--- TP1/TriggersExams.sql
+++ TP1/TriggersExams.sql
@@ -3,9 +3,9 @@ USE exams;
 CREATE TRIGGER move_to_wl
 AFTER UPDATE ON exam
 FOR EACH ROW
-INSERT INTO worklist
-SELECT NEW.idRequest,NEW.State,NEW.Date,
-		NEW.Medical_Act,NEW.User_idUser,NEW.Report,NEW.Notes
-        Name,idProcess,Address,Mobile
+INSERT INTO worklist 
+SELECT NULL,NEW.idRequest,NEW.State,NEW.Date,
+		NEW.Medical_Act,NEW.idUser,Name,idProcess,
+        Address,Mobile,NEW.Notes,NEW.Report
 FROM user
 WHERE NEW.User_idUser = user.idUser;
