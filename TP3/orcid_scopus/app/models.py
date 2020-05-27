8e450d24000b1995b310e4397bf237638b396794
Henrique Pereira <pereirapalmeirahenrique@gmail.com>
2020-05-27 02:56:28 +0100

    first DB model draft

---
+++ TP3/orcid_scopus/app/models.py
@@ -0,0 +1,27 @@
+# DB MODEL:
+
+# investigator document:
+# {
+#   id,
+#   orcid_id,
+#   last_updated,
+#   name,
+#   publications: {
+#       title,
+#       year,
+#       local,
+#       scopus: {
+#           eid,
+#           type (null),
+#           authors,
+#           num_quotes,
+#           num_quotes_last_three_years,
+#           sjr 
+#       }
+#       webofscience: {
+#           wos,
+#           type (null)
+#       }
+#   }
+# }
+
